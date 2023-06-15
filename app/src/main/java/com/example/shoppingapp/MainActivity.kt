e85d4648fe6f4e54f776c4fb68871b79860adc26
Shivangi <shivangit1111@gmail.com>
2023-06-15 16:37:39 +0530

    feat: create splash screen

--- app/src/main/java/com/example/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppingapp/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.example.shoppingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.shoppingapp.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        activityScope.launch {
+            delay(2000)
+
+            val intent = Intent(this@MainActivity, LoginScreen::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
+    }
+
 }
