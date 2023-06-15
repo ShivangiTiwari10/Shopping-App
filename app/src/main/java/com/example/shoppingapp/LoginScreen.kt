d1d5190e93c693149da6b0569f9b160d2425e2af
Shivangi <shivangit1111@gmail.com>
2023-06-15 17:44:36 +0530

    build: add buildScript

--- app/src/main/java/com/example/shoppingapp/LoginScreen.kt
+++ app/src/main/java/com/example/shoppingapp/LoginScreen.kt
@@ -9,7 +9,6 @@ class LoginScreen : AppCompatActivity() {
     private lateinit var binding: ActivityLoginScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_screen)
 
         binding = ActivityLoginScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
