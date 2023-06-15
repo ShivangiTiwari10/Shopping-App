482f1ac27728bd95b3b09ef2d91255ca0a5337db
Shivangi <shivangit1111@gmail.com>
2023-06-15 17:26:22 +0530

    feat: bind view

--- app/src/main/java/com/example/shoppingapp/LoginScreen.kt
+++ app/src/main/java/com/example/shoppingapp/LoginScreen.kt
@@ -2,15 +2,18 @@ package com.example.shoppingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shoppingapp.databinding.ActivityLoginScreenBinding
 
 class LoginScreen : AppCompatActivity() {
 
-
-
+    private lateinit var binding: ActivityLoginScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
 
+        binding = ActivityLoginScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar!!.hide()
+
     }
 }
