017241b7951d2abf40bf5427e1e1f465afd1e32a
Shivangi <shivangit1111@gmail.com>
2023-06-15 17:18:49 +0530

    feat: create splash screen

--- app/src/main/java/com/example/shoppingapp/LoginScreen.kt
+++ app/src/main/java/com/example/shoppingapp/LoginScreen.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class LoginScreen : AppCompatActivity() {
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
