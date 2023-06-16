969a107ed45016b959f5bdcefecc5c2372a80a66
Shivangi <shivangit1111@gmail.com>
2023-06-16 15:43:20 +0530

    feat: create bottom navigating bar

--- app/src/main/java/com/example/shoppingapp/BottomBarActivity.kt
+++ app/src/main/java/com/example/shoppingapp/BottomBarActivity.kt
@@ -2,10 +2,59 @@ package com.example.shoppingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.fragment.findNavController
+import com.example.shoppingapp.databinding.ActivityBottomBarBinding
 
 class BottomBarActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityBottomBarBinding
+
+    var i = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bottom_bar)
+
+        binding = ActivityBottomBarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_nav)
+
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
+
+        binding.bottomBar.onItemSelected = {
+            when (it) {
+                0 -> {
+                    i = 0
+                    navController.navigate(R.id.homeFragment)
+                }
+                1 -> i = 1
+                2 -> i = 2
+            }
+        }
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+
+
+            title = when (destination.id) {
+                R.id.cardFragment -> "MyCart"
+                R.id.moreFragment -> "MyDashBoard"
+                else -> " p-Kart"
+            }
+        }
     }
+
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (i == 0) {
+            finish()
+        }
+
+    }
+
 }
