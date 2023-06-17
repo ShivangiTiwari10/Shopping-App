a4b8f52a6398a9e59766655c97180bb4395cbc9c
Shivangi <shivangit1111@gmail.com>
2023-06-17 19:55:52 +0530

    feat: logIn registered user

--- app/src/main/java/com/example/shoppingapp/LoginScreen.kt
+++ app/src/main/java/com/example/shoppingapp/LoginScreen.kt
@@ -1,8 +1,18 @@
 package com.example.shoppingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.shoppingapp.databinding.ActivityLoginScreenBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class LoginScreen : AppCompatActivity() {
 
@@ -14,5 +24,64 @@ class LoginScreen : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar!!.hide()
 
+
+        binding.button2.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
+
+        binding.button.setOnClickListener {
+            if (binding.userNumber.text!!.isEmpty())
+                Toast.makeText(this, "Please Provide Number", Toast.LENGTH_SHORT).show()
+            else
+                sendOtp(binding.userNumber.text.toString())
+        }
+    }
+
+    private lateinit var builder: AlertDialog
+
+    private fun sendOtp(number: String) {
+
+        builder = AlertDialog.Builder(this)
+            .setTitle("Loading...")
+            .setMessage("Please Wait..")
+            .setCancelable(false)
+            .create()
+        builder.show()
+
+
+        val options = PhoneAuthOptions.newBuilder(FirebaseAuth.getInstance())
+            .setPhoneNumber("+91$number")       // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this)                 // Activity (for callback binding)
+            .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+
+    }
+
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            Log.d("Verified", "$credential")
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+
+            Log.d("NotVerified", "${e.message}")
+        }
+
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken
+        ) {
+            builder.dismiss()
+            val intent = Intent(this@LoginScreen, OTPActivity::class.java)
+            intent.putExtra("verificationId", verificationId)
+            intent.putExtra("number", binding.userNumber.text.toString())
+            startActivity(intent)
+        }
     }
 }
+
--- app/src/main/java/com/example/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppingapp/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         activityScope.launch {
             delay(2000)
 
-            val intent = Intent(this@MainActivity, BottomBarActivity::class.java)
+            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
             startActivity(intent)
             finish()
         }
