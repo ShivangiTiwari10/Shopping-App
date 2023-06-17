4f238a985498ead9748d03a4aa9a04034f2e2a51
Shivangi <shivangit1111@gmail.com>
2023-06-17 19:54:20 +0530

    style: design layout

---
+++ app/src/main/java/com/example/shoppingapp/OTPActivity.kt
@@ -0,0 +1,18 @@
+package com.example.shoppingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shoppingapp.databinding.ActivityOtpactivityBinding
+
+class OTPActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOtpactivityBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityOtpactivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+    }
+}
