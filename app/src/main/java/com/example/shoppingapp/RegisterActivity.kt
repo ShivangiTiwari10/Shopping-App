6bdcede18983295a7e29aacf6937bfe772edd032
Shivangi <shivangit1111@gmail.com>
2023-06-17 19:56:07 +0530

    feat: hide action bar

--- app/src/main/java/com/example/shoppingapp/RegisterActivity.kt
+++ app/src/main/java/com/example/shoppingapp/RegisterActivity.kt
@@ -18,6 +18,7 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.hide()
 
         binding.button.setOnClickListener {
 //            To register New user
